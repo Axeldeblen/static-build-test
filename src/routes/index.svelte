d541281d8e439a2b0b0248b39d8917eed51e84c8
Axel <67882025+Axeldeblen@users.noreply.github.com>
2022-02-01 10:11:08 +1100

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		console.log('mount');
+		console.log($page.url.searchParams.toString());
+		console.log($page.url.searchParams.get('tag'));
+	});
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<a href="/new/?tag=navigated">tag = navigated</a>
---
+++ src/routes/new.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		console.log('mount');
+		console.log($page.url.searchParams.toString());
+		console.log($page.url.searchParams.get('tag'));
+	});
+</script>
+
+<a href="/?tag=backToHome">tag = backToHome</a>
